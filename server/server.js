b567e71751111a3b1bdae2d184cde225ca1e80a9
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-24 16:17:52 -0500

    added funstionality to add to DB with Knex

--- server/server.js
+++ server/server.js
@@ -6,20 +6,56 @@ const knex = require("knex")(config);
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const port = process.env.PORT || 3000
-const app = express()
+const encoderDMP = require("../lib/analysis/DMPencoder.js");
 
-app.use(express.static("client"))
-app.use(bodyParser.json())
+const port = process.env.PORT || 3000;
+const app = express();
 
-app.set("port", port)
+app.use(express.static("client"));
+app.use(bodyParser.json());
+
+app.set("port", port);
 
 app.post("/api/newFlow", (req, res) => {
+	const flow = req.body.flow;
+	const artist = req.body.artist;
+	const album = {title: req.body.album};
+	const track = {title: req.body.track};
+
+	//insert artist name into artist table
+	knex("Artist")
+		.insert({name: artist})
+		.then((data) => {
+			res.json(data);
+		})
+	//insert album info
+	knex("Album")
+		.insert({title: album.title})
+		.then((data) => {
+			res.json(data);
+		})
+	//insert album info
+	knex("Track")
+		.insert({title: track.title})
+		.then((data) => {
+			res.json(data);
+		})
+	//insert raw flow into flow table
 	knex("Raw")
 		.insert(req.body)
 		.then((data) => {
 			res.json(data);
 		})
+
+	// const encodedObj = {flow:	encoderDMP(flow)};
+
+	//insert DMP flow into DMP table
+	knex("DMP")
+		.insert({flow:	encoderDMP(flow)})
+		.then((data) => {
+			res.json(data);
+		})
+		
 });
 
 app.listen(port, () => {
