63ee3522e4965187f7902eeb09146a9651848988
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-24 12:50:30 -0500

    changed to express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const config = require("../database/knexfile.js").development;
+const knex = require("knex")(config);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(express.static("client"))
+app.use(bodyParser.json())
+
+app.set("port", port)
+
+
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+})
