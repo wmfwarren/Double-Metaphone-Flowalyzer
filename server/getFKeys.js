0e040e59424eda6d9f77e74119200b05d799c8e4
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-25 11:40:26 -0500

    started functions to get PKs

---
+++ server/getFKeys.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const config = require("../database/knexfile.js").development;
+const knex = require("knex")(config);
+
+
+const getArtistId = (artistName) => {
+
+	return new Promise ((resolve, reject) => {
+
+		knex("Artist").where("name", artistName).select("id").then((data) => {
+			if (data) {
+				resolve(data);
+			} else {
+				reject();
+			}
+		});
+	})
+};
+
+const getAlbumId = (albumName) => {
+
+	return new Promise ((resolve, reject) => {
+
+		knex("Album").where("title", albumName).select("id").then((data) => {
+			if (data) {
+				resolve(data);
+			} else {
+				reject();
+			}
+		});
+	})
+};
+
+module.exports = {getArtistId, getAlbumId};
