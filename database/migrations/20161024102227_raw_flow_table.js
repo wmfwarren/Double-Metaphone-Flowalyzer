a4d0f644f290f94d1a4efc45b49f6e513e42c72d
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-29 17:57:00 -0500

    doubled the length of max flows to accommodate extra long flows such as Kanyes

--- database/migrations/20161024102227_raw_flow_table.js
+++ database/migrations/20161024102227_raw_flow_table.js
@@ -3,7 +3,7 @@
 module.exports.up = (knex, Promise) => {
   return knex.schema.createTable('Raw', (table) => {
         table.increments("id"); //for the primary key UID 
-        table.string('flow', 2048);
+        table.string('flow', 4095);
         table.integer("length");
         table.integer("unique_words");
     })
