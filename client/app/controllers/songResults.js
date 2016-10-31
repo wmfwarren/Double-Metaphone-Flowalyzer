85612d6d6dc20c1ef423a82641db6c0c7bd1cbb3
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-31 15:42:31 -0500

    started work on the graph

--- client/app/controllers/songResults.js
+++ client/app/controllers/songResults.js
@@ -4,8 +4,40 @@ app.controller("SongResultsCtrl", ["$scope", "$http", "searchDataFactory", funct
 
 	$scope.songInfo = searchDataFactory.getSearchData();
 
-	console.log("$scope.songInfo", $scope.songInfo);
+	console.log("$scope.songInfo", $scope.songInfo.data);
 
-	
+	//D3 Graph Work
+	const BAR_HEIGHT = 25;
+	const WIDTH = 400;
+
+	 var yAxis = d3.axisLeft()
+      .scale(6);
+
+  var xAxis = d3.axisBottom()
+      .scale($scope.songInfo.data.length + 1);
+
+	const dataArray = [];
+
+	for(let i = 0; i < $scope.songInfo.data.length; i++) {
+		dataArray.push($scope.songInfo.data[i].avg);
+	}
+
+	const x = d3.scaleLinear()
+		.domain([0, d3.max(dataArray)])
+		.range([0, WIDTH]);
+
+	const chart = d3.select("#flowDataGraph")
+		.attr("width", WIDTH)
+    .attr("height", BAR_HEIGHT * dataArray.length);
+
+  const bar = chart.selectAll("g")
+    .data(dataArray)
+  	.enter()
+  	.append("g")
+    .attr("transform", function(d, i) { return "translate(0," + i * BAR_HEIGHT + ")"; });
+
+  bar.append("rect")
+    .attr("width", x)
+    .attr("height", BAR_HEIGHT - 1);
 
 }]);
