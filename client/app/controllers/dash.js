07983a560be81565811bef453e650083b65a2219
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-30 22:58:51 -0500

    continued work on geting data back to the DOM

--- client/app/controllers/dash.js
+++ client/app/controllers/dash.js
@@ -1,7 +1,25 @@
 "use strict";
 
-app.controller("DashCtrl", ["$scope", function($scope) {
+app.controller("DashCtrl", ["$scope", "$http", function($scope, $http) {
  	$scope.metricSummary = null;
+ 	$scope.dashboardInfo = null;
+
+ 	$scope.getFlowLengths = () => {
+ 		$scope.metricSummary = "These are the three rappers with the longest flows."
+
+ 		$http.get("/api/averageLengths")
+ 			.then((lengthData) => {
+ 				$scope.dashboardInfo = lengthData.data;
+ 			})
+ 	};	
+
+ 	$scope.getFlowUniqueness = () => {
+ 		$scope.metricSummary = "These are the rappers with the most discrete words in their flows. This is calculated by dividing unique words by length."
+ 	};	
+
+ 	$scope.getFlowWeightedUniqueness = () => {
+ 		$scope.metricSummary = "The longer a flow gets the less unique it will get by nature due to more words like articles and prepositions being reused. This is the uniqueness value times a fudge factor determined by length."
+ 	};
 
 
 }]);
