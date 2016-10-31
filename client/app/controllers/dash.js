ace945d6429a272c981cb6293a0f76fbc47f68ad
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-30 23:13:18 -0500

    you can now see lengths and uniquenesses of flows on the dash

--- client/app/controllers/dash.js
+++ client/app/controllers/dash.js
@@ -3,9 +3,11 @@
 app.controller("DashCtrl", ["$scope", "$http", function($scope, $http) {
  	$scope.metricSummary = null;
  	$scope.dashboardInfo = null;
+ 	$scope.dataQuery = null;
 
  	$scope.getFlowLengths = () => {
  		$scope.metricSummary = "These are the three rappers with the longest flows."
+ 		$scope.dataQuery = "length";
 
  		$http.get("/api/averageLengths")
  			.then((lengthData) => {
@@ -15,6 +17,13 @@ app.controller("DashCtrl", ["$scope", "$http", function($scope, $http) {
 
  	$scope.getFlowUniqueness = () => {
  		$scope.metricSummary = "These are the rappers with the most discrete words in their flows. This is calculated by dividing unique words by length."
+ 		$scope.dataQuery = "unique";
+
+ 		$http.get("/api/averageUniqueness")
+ 			.then((uniquenessData) => {
+ 				$scope.dashboardInfo = uniquenessData.data;
+ 				console.log("$scope.dashboardInfo", $scope.dashboardInfo);
+ 			})
  	};	
 
  	$scope.getFlowWeightedUniqueness = () => {
