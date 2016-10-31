18a38005a0f4ddb8a861420b4e00c67f53a7692c
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-30 22:18:01 -0500

    started getting data from navebar search bar and PG SQL

--- client/app/controllers/nav.js
+++ client/app/controllers/nav.js
@@ -1,5 +1,15 @@
 "use strict";
 
-app.controller("NavCtrl", ["$scope", function($scope) {
- 
+app.controller("NavCtrl", ["$scope", "$http", function($scope, $http) {
+	$scope.searchTerm = null;
+
+	$scope.searchArtist = () => {
+
+		$http.post("/api/searchArtistFlows", {searchTerm: $scope.searchTerm})
+			.then((data) => {
+				console.log("Data", data);
+			})
+			.catch(console.error);
+	}
+
 }]);
