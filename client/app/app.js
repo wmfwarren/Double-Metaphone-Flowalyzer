d355fb5e43bed70a18d6645fe41eaeb40ef52688
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-30 19:18:19 -0500

    started app config

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const app = angular.module("flowalyzer", ["ngRoute"]);
+
+app.config(["$routeProvider", function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: "DashCtrl",
+      templateUrl: "/app/partials/dash.html"
+    })
+}]);
+
+
+
