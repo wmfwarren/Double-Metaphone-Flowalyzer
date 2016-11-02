f021f9b66044e3944fce2fdd70c0cf4edd622ad2
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-11-01 19:16:47 -0500

    Added iife to automatically get data for rapper names on page load

--- client/app/controllers/dash.js
+++ client/app/controllers/dash.js
@@ -3,10 +3,19 @@
 app.controller("DashCtrl", ["$scope", "$http", "$location", "searchDataFactory", function($scope, $http, $location, searchDataFactory) {
  	$scope.metricSummary = "Click a metric for more information.";
  	$scope.dashboardInfo = null;
- 	$scope.dataQuery = null;
+ 	$scope.dataQuery = "default";
+
+
+ 	$scope.getFlowLengths = (() => {
+
+ 		$http.get("/api/averageLengths")
+ 			.then((lengthData) => {
+ 				$scope.dashboardInfo = lengthData.data;
+ 			})
+ 	})();	
 
  	$scope.getFlowLengths = () => {
- 		$scope.metricSummary = "These are the three rappers with the longest flows. This is words per flow."
+ 		$scope.metricSummary = "These are the three rappers with the longest flows. While length can be impressive by necessitating compliance to rhymes or metaphors for longer, some rappers favor more complex short flows."
  		$scope.dataQuery = "length";
 
  		$http.get("/api/averageLengths")
@@ -16,7 +25,7 @@ app.controller("DashCtrl", ["$scope", "$http", "$location", "searchDataFactory",
  	};	
 
  	$scope.getFlowUniqueness = () => {
- 		$scope.metricSummary = "These are the rappers with the most discrete words in their flows. This is calculated by dividing unique words by the length of the flow."
+ 		$scope.metricSummary = "These are the rappers with the most discrete words in their flows. This metric usually is lower for rappers who favor longer flows, because of the necessary reuse of words like articles, or those using poetic repetition in their flows."
  		$scope.dataQuery = "unique";
 
  		$http.get("/api/averageUniqueness")
@@ -26,7 +35,7 @@ app.controller("DashCtrl", ["$scope", "$http", "$location", "searchDataFactory",
  	};	
 
  	$scope.getAvgWordLength = () => {
- 		$scope.metricSummary = "A measure of the average length of words. For further data search for an artists flows!"
+ 		$scope.metricSummary = "A measure of the average length of words. Longer works can give rise to more complex rhyme schemes, but not always."
  		$scope.dataQuery = "wordLengths";
 
  		$http.get("/api/averageWordLengths")
