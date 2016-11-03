82a01f9630ecb6ee62324b73d5eb4cc4bf146083
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-11-02 21:32:05 -0500

    find array works

--- client/app/controllers/songInput.js
+++ client/app/controllers/songInput.js
@@ -2,18 +2,58 @@
 
 app.controller("SongInputCtrl", ["$scope", "$http", "$location", "searchDataFactory", function($scope, $http, $location, searchDataFactory) {
 
-	$scope.flowText = null;
-	$scope.track = null;
-	$scope.artist = null;
-	$scope.rapper = null;
-	$scope.album = null;
+	$scope.flowText = "";
+	$scope.track = "";
+	$scope.artist = "";
+	$scope.rapper = "";
+	$scope.album = "";
 
 	$scope.inputFlow = () => {
 		let artistExists = false;
 		let albumExists = false;
 		let trackExists = false;
 
-		
+		const promiseArray = [];
+
+		promiseArray.push(
+				$http.post("/api/findArtist", {artist: $scope.artist})
+					.then((result) => {
+						if(result.data.length > 0){
+							return true;
+						}
+
+						return false;
+					});
+		);	
+
+		promiseArray.push(
+				$http.post("/api/findAlbum", {album: $scope.album})
+					.then((result) => {
+						if(result.data.length > 0){
+							return true;
+						}
+
+						return false;
+					});
+		);	
+
+		promiseArray.push(
+				$http.post("/api/findTrack", {track: $scope.track})
+					.then((result) => {
+						if(result.data.length > 0){
+							return true;
+						}
+
+						return false;
+					});
+		);
+
+		console.log("prom", promiseArray);
+
+		Promise.all(promiseArray)
+			.then((values) => {
+				
+			});
 
 	}
 
