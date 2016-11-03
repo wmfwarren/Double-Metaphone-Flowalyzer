172d551bc40018570fe9dab4cf3e443b809577e2
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-11-02 21:49:57 -0500

    almost got posting

--- client/app/controllers/songInput.js
+++ client/app/controllers/songInput.js
@@ -9,51 +9,74 @@ app.controller("SongInputCtrl", ["$scope", "$http", "$location", "searchDataFact
 	$scope.album = "";
 
 	$scope.inputFlow = () => {
-		let artistExists = false;
-		let albumExists = false;
-		let trackExists = false;
-
 		const promiseArray = [];
 
-		promiseArray.push(
-				$http.post("/api/findArtist", {artist: $scope.artist})
-					.then((result) => {
-						if(result.data.length > 0){
-							return true;
-						}
+		if($scope.track && $scope.artist && $scope.album && $scope.rapper) {
+			promiseArray.push(
+					$http.post("/api/findArtist", {artist: $scope.artist})
+						.then((result) => {
+							if(result.data.length > 0){
+								return true;
+							}
 
-						return false;
-					});
-		);	
+							return false;
+						})
+			);	
 
-		promiseArray.push(
-				$http.post("/api/findAlbum", {album: $scope.album})
-					.then((result) => {
-						if(result.data.length > 0){
-							return true;
-						}
+			promiseArray.push(
+					$http.post("/api/findAlbum", {album: $scope.album})
+						.then((result) => {
+							if(result.data.length > 0){
+								return true;
+							}
 
-						return false;
-					});
-		);	
+							return false;
+						})
+			);	
 
-		promiseArray.push(
-				$http.post("/api/findTrack", {track: $scope.track})
-					.then((result) => {
-						if(result.data.length > 0){
-							return true;
-						}
+			promiseArray.push(
+					$http.post("/api/findTrack", {track: $scope.track})
+						.then((result) => {
+							if(result.data.length > 0){
+								return true;
+							}
 
-						return false;
-					});
-		);
+							return false;
+						})
+			);
+		}
 
 		console.log("prom", promiseArray);
+		if(promiseArray.length === 3){
+			Promise.all(promiseArray)
+				.then((values) => {
+					console.log("values", values);
+					if(!values[0]) {
+						$http.post("/api/newArtist", {artist: $scope.artist})
+							.then((data) => {
+								console.log("artistExists", data );
+							})
+					}
+					if(!values[1]) {
+						$http.post("/api/newAlbum", {artist: $scope.artist, album: $scope.album})
+							.then((data) => {
+								console.log("albumExists", data );
+							})
+					}
+					if(!values[2]) {
+						$http.post("/api/newTrack", {track: $scope.track, album: $scope.album})
+							.then((data) => {
+								console.log("trackExists", data );
+							})
+					}
 
-		Promise.all(promiseArray)
-			.then((values) => {
-				
-			});
+					$http.post("/api/newFlow", {flow: $scope.flowText})
+						.then((data) => {
+							console.log("flowExists", data );
+						})
+
+				});
+		}
 
 	}
 
