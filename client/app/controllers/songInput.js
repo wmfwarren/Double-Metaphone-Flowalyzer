8ef08f5fef92e79ea04693ed4cd8d881f9057e41
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-11-02 20:57:44 -0500

    started working on browers data input

---
+++ client/app/controllers/songInput.js
@@ -0,0 +1,20 @@
+"use strict";
+
+app.controller("SongInputCtrl", ["$scope", "$http", "$location", "searchDataFactory", function($scope, $http, $location, searchDataFactory) {
+
+	$scope.flowText = null;
+	$scope.track = null;
+	$scope.artist = null;
+	$scope.rapper = null;
+	$scope.album = null;
+
+	$scope.inputFlow = () => {
+		let artistExists = false;
+		let albumExists = false;
+		let trackExists = false;
+
+		
+
+	}
+
+}]);
