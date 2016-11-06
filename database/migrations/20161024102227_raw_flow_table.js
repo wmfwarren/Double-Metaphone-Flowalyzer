12bd39b0109db4ea7797ceec9ec735af15d26caf
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-11-06 14:22:17 -0600

    added stats to the table for line length

--- database/migrations/20161024102227_raw_flow_table.js
+++ database/migrations/20161024102227_raw_flow_table.js
@@ -6,13 +6,18 @@ module.exports.up = (knex, Promise) => {
         table.string('flow', 4095);
         table.integer("length");
         table.integer("unique_words");
+        //lord length info
         table.float("average_word_length");
         table.float("word_length_stdev");
         table.float("word_percent_rsd");
         table.integer("mode_word_length");
         table.float("median_word_length");
+        //line info
         table.integer("number_of_lines");
         table.float("mean_words_by_line");
+        table.integer("mode_words_by_line");
+        table.float("median_words_by_line");
+        table.float("stdev_words_by_line");
     })
 };
 
